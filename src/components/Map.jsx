19f24f12a7e811089e5a0dd3bcd04a4494e29a85
jasmine carpentier <jascarpentier@gmail.com>
2019-07-29 15:54:34 -0400

    setup

---
+++ src/components/Map.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Map extends React.Component {
+  render() {
+    return (
+      <div className='map-section'>
+
+        <h4>Hours & Location</h4>
+
+        <h5>Tuesday - Friday</h5>
+        <p>11am-10pm</p>
+
+        <h5>Saturday</h5>
+        <p>12pm-10pm</p>
+
+        <h5>Sunday</h5>
+        <p>1pm-9pm</p>
+
+        <h5>Closed Mondays</h5>
+
+        <p>220-20 Hillside Ave. Queens Villiage, NY 11427</p>
+
+        <h5>718.749.9118</h5>
+
+      </div>
+    )
+  }
+}
+
+export default Map
