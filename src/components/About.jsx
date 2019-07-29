65abde61956b61c9d2e4fdaae32e196ff2302d44
Misha Kessler <misha.kessler@gmail.com>
2019-07-29 18:25:50 -0400

    Add div's to submenus; lint classes to functional components.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,8 +7,8 @@ class About extends React.Component {
   render() {
     return (
       <div className="about">
-        <img src={about1} alt="family picture" />
-        <img src={about2} alt="making pizza dough" />
+        <img src={about1} alt="Family making dough at Cara Mia." />
+        <img src={about2} alt="Handkneading pizza dough at Cara Mia." />
         <p>It all began many years ago in a small region in Italy called Broccostella, where Tomaso and Armanda DeCiantis dreamed of moving to the United States to start their own business. After arriving at Ellis Island and much hard work, they opened Cara Mia Restaurant in Queens Village, New York. Both their children (Larry, Carlo and Anna) and Armanda’s mother, Liberata helped out every day in the restaurant. Cara Mia soon became famous for Liberata’s meatball recipe. All three children can still be found here preparing our fresh Italian specialties.</p>
       </div>
     )
