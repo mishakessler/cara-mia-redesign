8b9631cbb4a3ed6471d7c180101a74a38695e36a
jasmine carpentier <jascarpentier@gmail.com>
2019-07-29 15:34:25 -0400

    text done

--- src/components/menu/PrivateParty.jsx
+++ src/components/menu/PrivateParty.jsx
@@ -4,8 +4,56 @@ class PrivateParty extends React.Component {
   render() {
     return (
       <div className='submenu'>
-        <h1>PRIVATE PARTY</h1>
+        <h3>PRIVATE PARTY</h3>
 
+        <h4>Party Menu 1</h4>
+
+        <h5>First Course - Salad</h5>
+        <h5>Family Style Salad</h5>
+        <p>(Mixed or Caesar)</p>
+
+        <h5>Second Course - Pasta</h5>
+        <h5>Family Style Penne</h5>
+        <p>(Choice of Voka or Marinara)</p>
+
+        <h5>Main Course - Choice</h5>
+        <h5>Chicken Francaise</h5>
+        <h5>Flounder Oreganata</h5>
+        <h5>Veal Parmigiana</h5>
+        <h5>Eggplant Rollatini</h5>
+
+        <h5>Includes:</h5>
+        <h5>Unlimited Soda, Coffee and Tea</h5>
+        <p>Choice of connoli, cheesecake or tortoni</p>
+
+        <h6>35$ per person</h6>
+        <h6>Tax and 20% gratuity will be added</h6>
+
+        <h4>Party Menu 2</h4>
+
+        <h5>First Course - Salad</h5>
+        <h5>Family Style Hot Appetizers</h5>
+        <p>Fried Calamari, Clams, Mozzerella Sticks and Stuffed Mushrooms</p>
+
+        <h5>Second Course - Pasta</h5>
+        <h5> Family Style Penne alla Vodka and Penne Marinara</h5>
+
+        <h5>Main Course - Entrée Choice</h5>
+        <p>Veal Parmigana</p>
+        <p>Chicken Francaise</p>
+        <p>Salmon Oreganata</p>
+        <p>Chicken Parmigiana</p>
+        <p>Eggplant Rollatini</p>
+
+        <h5>Includes:</h5>
+        <h5>Unlimited Soda, Coffee, Tea, Cappuccino and Espresso</h5>
+        <h5>Homemade Cannoli, Tartufo, Cheesecake</h5>
+
+        <h6>45$ per person</h6>
+
+        <h6>Tax & 20% gratuity will be added</h6>
+
+        <h5>Please inquire about custom menu's and liquor packages</h5>
 
       </div>
     )
