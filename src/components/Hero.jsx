d4ff5b64e3ca89ec8ca1c01d57976b9ffcfb113f
Christopher Montalbo <montyrivers@Christophers-MacBook-Pro-2.local>
2019-07-29 16:33:55 -0400

    started Hero.jsx added more Dinner.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <div>
+        <button>Reservations</button>
+        <button>Catering</button>
+      </div>
+      <div>
+        <p>Traditional Italian
+          Restaurant Specializing
+          In Roman Trattoria Fare
+        </p>
+      </div>
+    </div>
+  )
+}
