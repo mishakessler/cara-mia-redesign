85f14f2a5de78f3bf5a0d7443f2c391c701aa924
jasmine carpentier <jascarpentier@gmail.com>
2019-07-29 15:13:59 -0400

    init

---
+++ src/components/menu/PrivateParty.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PrivateParty extends React.Component {
+  render() {
+    return (
+      <div className='submenu'>
+        <h1>PRIVATE PARTY</h1>
+
+
+      </div>
+    )
+  }
+}
+
+export default PrivateParty
