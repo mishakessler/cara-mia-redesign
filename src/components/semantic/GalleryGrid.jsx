a191c5e5e28414d89e3137f3de8be021ab73086b
anumhsn22 <anumh123@gmail.com>
2019-07-31 16:13:50 -0400

    styled gallery and map section

--- src/components/semantic/GalleryGrid.jsx
+++ src/components/semantic/GalleryGrid.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import { Grid, Image } from 'semantic-ui-react'
-import about1 from '../../assets/about1.png'
+import Gallery1 from '../../assets/Gallery1.png'
+import Gallery2 from '../../assets/Gallery2.png'
+import Gallery3 from '../../assets/Gallery3.png'
+import Gallery4 from '../../assets/Gallery4.png'
+import Gallery5 from '../../assets/Gallery5.png'
+import Gallery6 from '../../assets/Gallery6.png'
+
 
 const GalleryGrid = () => (
   <Grid>
     <Grid.Row columns={3}>
       <Grid.Column>
-        <Image src={about1} />
+        <Image src={Gallery1} />
       </Grid.Column>
       <Grid.Column>
-        <Image src={about1} />
+        <Image src={Gallery2} />
       </Grid.Column>
       <Grid.Column>
-        <Image src={about1} />
+        <Image src={Gallery3} />
+      </Grid.Column>
+    </Grid.Row>
+    <Grid.Row columns={3}>
+      <Grid.Column>
+        <Image src={Gallery4} />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src={Gallery5} />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src={Gallery6} />
       </Grid.Column>
     </Grid.Row>
   </Grid>
